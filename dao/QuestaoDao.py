655b7fc857cb8154c7aa078c30efb5cb0f77442b
Jefferson Lessa <jefferson2459@gmail.com>
2019-02-21 16:19:20 -0300

    Tratando Exception

--- dao/QuestaoDao.py
+++ dao/QuestaoDao.py
@@ -5,51 +5,80 @@ from model.Questao import *
 class QuestaoDao:
 
     def __init__(self):
-        db.connect()
-        if not Questao.table_exists():
-            db.create_tables([Questao])
-        db.close()
+        try:
+            db.connect()
+            if not Questao.table_exists():
+                db.create_tables([Questao])
+            # db.close()
+        except Exception as e:
+            print(e)
+        finally:
+            if not db.is_closed():
+                db.close()
 
     def questao_save(self, questao_model):
-        db.connect()
-        if not Questao.table_exists():
-            db.create_tables([Questao])
-        questao_model.save()
-        db.close()
+        try:
+            db.connect()
+            if not Questao.table_exists():
+                db.create_tables([Questao])
+            questao_model.save()
+        except Exception as e:
+            print('e')
+        finally:
+            db.close()
 
     def remove_questao(self, questao):
-        db.connect()
-        query = Questao.delete().where(Questao.id == questao.id)
-        query2 = Imagem.delete().where(Imagem.questao_id == questao.id)
-        query.execute()
-        query2.execute()
-        db.close()
+        try:
+            db.connect()
+            query = Questao.delete().where(Questao.id == questao.id)
+            query2 = Imagem.delete().where(Imagem.questao_id == questao.id)
+            query.execute()
+            query2.execute()
+        except Exception as e:
+            print('e')
+        finally:
+            db.close()
 
     def questao_get_by_concurso(self, concurso):
-        db.connect()
-        questao = Questao.get(Questao.concurso == concurso)
-        db.close()
+        try:
+            db.connect()
+            questao = Questao.get(Questao.concurso == concurso)
+        except Exception as e:
+            print('e')
+        finally:
+            db.close()
         return questao
 
     def questao_get_by_enunciado(self, enunciado):
-        db.connect()
-        questao = Questao.get(Questao.enunciado == enunciado)
-        db.close()
+        try:
+            db.connect()
+            questao = Questao.get(Questao.enunciado == enunciado)
+        except Exception as e:
+            print('e')
+        finally:
+            db.close()
         return questao
 
     def questao_get_by_site_id(self, site_id):
         try:
             db.connect()
             questao = Questao.get(Questao.site_id == site_id)
-            db.close()
             return questao
         except Exception as e:
+            print(e)
+            return None
+        finally:
             if not db.is_closed():
                 db.close()
-            return None
+
 
     def questao_paginate(self, page, per_page):
-        db.connect()
-        listQuestao = Questao.select().order_by(Questao.id).paginate(page, per_page)
-        db.close()
+        try:
+            db.connect()
+            listQuestao = Questao.select().order_by(Questao.id).paginate(page, per_page)
+        except Exception as e:
+            print('e')
+        finally:
+            db.close()
+
         return listQuestao
